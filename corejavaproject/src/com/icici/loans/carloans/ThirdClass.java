b7fddbf1e8131a95d8846f48d6c8971fc76c9425
krishnavenirallabandi <rallabandi.krishnaveni28@gmail.com>
2019-02-16 18:19:40 -0800

    I added hello

--- corejavaproject/src/com/icici/loans/carloans/ThirdClass.java
+++ corejavaproject/src/com/icici/loans/carloans/ThirdClass.java
@@ -4,8 +4,8 @@ public class ThirdClass {
 
 	public static void main(String[] args) {
 	System.out.println("This is my thaird class");
-	System.out.println("this class is used for java programs");
-
+	System.out.println("this class is uded for java progrms");
+	System.out.println("hello");
 	}
 
 }
