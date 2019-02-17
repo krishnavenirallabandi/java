b68415bdb1b5e685a666acbad081af128094da29
krishnavenirallabandi <rallabandi.krishnaveni28@gmail.com>
2019-02-16 17:38:23 -0800

    Revert "this is class is used for basic programs"
    This reverts commit 420d548544e57bedeaf7a09668a55242ba465069.

--- corejavaproject/src/com/icici/loans/carloans/ThirdClass.java
+++ corejavaproject/src/com/icici/loans/carloans/ThirdClass.java
@@ -4,7 +4,6 @@ public class ThirdClass {
 
 	public static void main(String[] args) {
 	System.out.println("This is my thaird class");
-	System.out.println("This class is i usr for writing basic programs");
 
 	}
 
