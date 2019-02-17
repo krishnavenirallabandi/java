5b7eb2dffed1e2fec2c43b26a9706856782ecc05
krishnavenirallabandi <rallabandi.krishnaveni28@gmail.com>
2019-02-16 18:16:43 -0800

    this class is used for java programs

--- corejavaproject/src/com/icici/loans/carloans/ThirdClass.java
+++ corejavaproject/src/com/icici/loans/carloans/ThirdClass.java
@@ -4,6 +4,7 @@ public class ThirdClass {
 
 	public static void main(String[] args) {
 	System.out.println("This is my thaird class");
+	System.out.println("this class is used for java programs");
 
 	}
 
